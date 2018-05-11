24aab0afc0f0f92309f9e28ebe8cb19cf8228cf5
D-oxytocin <karankoron7979@gmail.com>
2018-05-11 14:55:04 +0900

    Edit-view

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace1
   class Application < Rails::Application
-  	config.generators do |g|
+    config.generators do |g|
       g.javascripts false
       g.helper false
       g.test_framework false
