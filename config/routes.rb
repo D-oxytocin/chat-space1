42e5f2d6212713f3e0bf1534f469693452e82492
D-oxytocin <karankoron7979@gmail.com>
2018-05-12 17:05:19 +0900

    Configuration-rooting

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get 'messages' => 'messages#index'
 
   resources :users, only: [:edit, :update, :index]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
 end
