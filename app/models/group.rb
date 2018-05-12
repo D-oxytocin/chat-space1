79cb01fa6ca0a062d9e98e7b03a1f631459d407a
D-oxytocin <karankoron7979@gmail.com>
2018-05-12 19:38:46 +0900

    Edit-create-group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,5 @@
 class Group < ApplicationRecord
-  has_many :messages
-  has_many :mebmers
-  belongs_to :users, through: :members
+  has_many :members
+  has_many :users, through: :members
   validates :name, presence: true
 end
