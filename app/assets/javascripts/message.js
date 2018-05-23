bfb12f005a8039e84791ae649e9a2551af6d33f1
D-oxytocin <karankoron7979@gmail.com>
2018-05-23 18:35:56 +0900

    Edit-indent

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,8 @@
 $(function(){
   function buildHTML(message){
     message.image.url == null ? image_html = `` : image_html = `<p class = "lower-message__image"><img src ="${message.image.url}" width="120" height="180"</p>`
-	var html =
-	  `<div class="message">
+    var html =
+      `<div class="message">
          <div class="upper-message">
            <div class="upper-message__user-name">
              ${message.user_name}
@@ -18,28 +18,28 @@ $(function(){
            ${image_html}
          </div>
        </div>`
-	return html;
+    return html;
   }
   $('#new_message').on('submit', function(e){
-  	e.preventDefault();
-  	var formData = new FormData(this);
-  	var url = $(this).attr('action')
-  	$.ajax({
-  	  url: url,
-  	  type: "POST",
-  	  data: formData,
-  	  dataType: 'json',
-  	  processData: false,
-  	  contentType: false,
-  	})
-  	.done(function(data){
-  	  var html = buildHTML(data);
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false,
+    })
+    .done(function(data){
+      var html = buildHTML(data);
       $('.messages').append(html)
       $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight}, 'fast');
       $('#new_message')[0].reset();
       $('.form__submit').prop('disabled', false);
-  	})
-  	.fail(function(){
+    })
+    .fail(function(){
       alert('error');
     })
   })
